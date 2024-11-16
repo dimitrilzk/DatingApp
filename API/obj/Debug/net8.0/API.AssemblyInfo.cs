b2817c366264338ce9c23d290b2e968936f62508
dimitrilzk <dimitrilazuka95@gmail.com>
2024-11-16 09:39:02 +0100

    add db, and first migration

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56cfab526e53217394a166e5f60419c9b5404c5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7ca6cdb43b529135b0ca28dbfee9d5bd82703e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
