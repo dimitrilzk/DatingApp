bc173eb6c0ea3103d84aca683d1ff72fb667e218
dimitrilzk <dimitrilazuka95@gmail.com>
2024-11-28 22:31:08 +0100

    aggiunto client http

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +9,24 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  //questo è un altro modo di fare la dep injection(alternativo al farlo nel costruttore)
+  http = inject(HttpClient);
   title = 'DatingApp';
+  users: any;
+  
+  //fare la dep inj nel costruttore è diventato antiquato 
+  //angular si sposta sempre più alla programmazione funzionale cit.
+  // constructor(private httpClient: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      // next: () => {},
+      next: response => this.users = response,
+      // error: () => {},
+      error: error => console.log(error),
+      // complete: () => {}
+      complete: () => console.log('Request has completed')
+    });
+  }
 }
