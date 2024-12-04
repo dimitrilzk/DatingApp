71342410d940f696897621af77d61a1b3011819e
dimitrilzk <dimitrilazuka95@gmail.com>
2024-12-04 21:14:57 +0100

    add base api controller

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        //DRY dont repeat yourself - uso questo controller per non ripetere la route e apiController
+    }
+}
