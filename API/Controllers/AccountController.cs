2d65ff3bbfcc924b5f91cd1382fe1a7cc417247e
dimitrilzk <dimitrilazuka95@gmail.com>
2024-12-05 20:54:03 +0100

    add dto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -8,21 +10,31 @@ namespace API.Controllers
 {
     public class AccountController(DataContext context) : BaseApiController
     {
+        //In the API Controllers we have an[ApiController] attribute.One of the features of this attribute
+        //is automatic property binding to the parameters of the endpoints.The basic rules are if the argument
+        //is a string property then it will look in the query string params, and if it is an object then
+        //it will look in the body of the request to match this.  This can be overridden if needed
+        //by supplying the [FromQuery] or[FromBody] attributes
         [HttpPost("register")] // api/account/register
-        public async Task<ActionResult<AppUser>> Register(string username, string password) // ep33
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto) // ep33
         // di default i parametri passati come stringhe vengono recuperati dalla query string
         // oppure lo si puo specificare: Register([FromQuery]string username, string password)
         // oppure si può indicare che i parametri sono da cercare nel body: Register([FromBody]string username, string password)
         // il modo migliore è comunque passare un oggetto (DTO) in questo modo di default l'oggetto verrà cercato nel Body ep34
         {
+            if(await UserExist(registerDto.Username))
+            {
+                return BadRequest("Username is taken");
+            }
+
             using var hmac = new HMACSHA512(); // usare lo using permette ri risparmiare memoria in quanto
             //appena si avrà finito di usare l'istanza della classe istanziata: new HMACSHA512() il metodo dispose
             //verrà chiamato e si occuperà di chiudere immediatamente lo scope e quindi non consumerà più memoria
 
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
 
@@ -31,6 +43,9 @@ namespace API.Controllers
 
             return user;
         }
-
+        private async Task<bool> UserExist(string username)
+        {
+            return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower()); //Bob != bob
+        }
     }
 }
---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]//valida la prop se non è null e se non è stringa vuota
+        public required string Username { get; set; }//questo required invece non fa vaildazione semplicemente il compilatore forzerà la sua inizializzazione
+        [Required]
+        public required string Password { get; set; }
+    }
+}
