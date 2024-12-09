2a43774a533ec5c7c6bfa63b49e4ec981de802cc
dimitrilzk <dimitrilazuka95@gmail.com>
2024-12-09 19:44:58 +0100

    add token service logic

--- API/Program.cs
+++ API/Program.cs
@@ -19,7 +19,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
-.WithOrigins("http://localhost:4200", "https://localhost:4200"));
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
