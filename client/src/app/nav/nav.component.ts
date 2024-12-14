5a3637c3b37a0e2a327f358bcce12afd36294b92
dimitrilzk <dimitrilazuka95@gmail.com>
2024-12-14 12:33:22 +0100

    angular - add ng forms in navbar

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
 })
 export class NavComponent {
+  //spiegazione [(ngModel)] 46: biding bidirezionale [()] se cambio da ts si modifica in html e viceversa
+  model: any = {};
 
+  login() {
+    console.log(this.model);
+  }
 }
