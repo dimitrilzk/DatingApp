7d67c655c792d9329611f068a469ecb04b83df9c
dimitrilzk <dimitrilazuka95@gmail.com>
2024-12-18 19:41:59 +0100

    angular-add home component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, NavComponent]
+  imports: [RouterOutlet, NavComponent, HomeComponent]
 })
 export class AppComponent implements OnInit {
   //questo è un altro modo di fare la dep injection(alternativo al farlo nel costruttore)
