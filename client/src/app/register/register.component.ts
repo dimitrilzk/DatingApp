9288cc36d59c8aee6ca4684185f062b0532b7f8c
Dimitri <dimitrilazuka95@gmail.com>
2025-01-10 22:42:57 +0100

    angular-implement register function

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
-import { Component, input, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -12,17 +13,24 @@ export class RegisterComponent {
   //passare dati da componente padre (home) a figlio (register) l.55 (vecchio modo)
   // @Input() usersFromHomeComponent: any;
   //il nuovo modo per passare dati da padre a figlio è questo: disponibile da verisone 17.3 di angular
-  usersFromHomeComponent = input.required<any>();
+  // usersFromHomeComponent = input.required<any>();
 
   //passare dati dal comoponente figlio al padre l.56 (vecchio modo)
   // @Output() cancelRegister = new EventEmitter();
   //il nuovo modo per passare dati dal figlio al padre ps non serve levent emitter
   cancelRegister = output<boolean>();
 
+  private accountService = inject(AccountService);
   model: any = {};
 
   register() {
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: error => console.log(error)
+    })
   }
 
   cancel() {
