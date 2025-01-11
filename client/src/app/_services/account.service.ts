2dd79fc0c91416aa0df8a62f91497e97561817d5
Dimitri <dimitrilazuka95@gmail.com>
2025-01-11 21:33:47 +0100

    fix

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -29,7 +29,7 @@ export class AccountService {
         if (user){
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUser.set(user);
-        }
+        };
         return user;
       })
     )
