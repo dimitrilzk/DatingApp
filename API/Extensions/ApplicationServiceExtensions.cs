201f689330275e7efcfe766608df431d762667da
dimitri_dev <dimitrilazuka95@gmail.com>
2025-02-26 10:00:11 +0100

    add photo service

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -8,6 +8,9 @@ namespace API.Extensions
 {
     public static class ApplicationServiceExtensions
     {
+        //AddTransient<IPhotoService, PhotoService>() → Nuova istanza ogni volta che viene richiesta.
+        //AddScoped<IPhotoService, PhotoService>() → Nuova istanza per ogni richiesta HTTP.
+        //AddSingleton<IPhotoService, PhotoService>() → Un'unica istanza per tutta la durata dell'applicazione.
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddControllers();
@@ -19,6 +22,7 @@ namespace API.Extensions
             //AddSingleton AddTransient AddScoped - ep 38
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();//ep 86 (end)
+            services.AddScoped<IPhotoService, PhotoService>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
 
