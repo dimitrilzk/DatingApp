235fe7cc9a498457b3c1b8dd197fe2057e3d2742
dimitri_dev <dimitrilazuka95@gmail.com>
2025-02-28 11:14:04 +0100

    return created at

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -86,7 +86,10 @@ public class UsersController(IUserRepository userRepository, IMapper mapper, IPh
 
         if(await userRepository.SaveAllAsync())
         {
-            return mapper.Map<PhotoDto>(photo);
+            //spiegazione ep 124
+            return CreatedAtAction(nameof(GetUser),
+                new { username = user.UserName },
+                mapper.Map<PhotoDto>(photo));
         }
 
         return BadRequest("Problem adding photo");
