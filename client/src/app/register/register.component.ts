0364f9f89afa49dd043d57d0b7abfb42b5bf36fc
dimitri_dev <dimitrilazuka95@gmail.com>
2025-03-09 09:27:43 +0100

    add custom validator - compare password

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,8 +1,10 @@
 import { Component, inject, OnInit, output } from '@angular/core';
 import {
+  AbstractControl,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
+  ValidatorFn,
   Validators,
 } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
@@ -44,8 +46,23 @@ export class RegisterComponent implements OnInit {
         Validators.minLength(4),
         Validators.maxLength(8),
       ]),
-      confirmPassword: new FormControl('', [Validators.required]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        this.matchValues('password'),
+      ]),
     });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () =>
+        this.registerForm.controls['confirmPassword'].updateValueAndValidity(),
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value
+        ? null
+        : { isMatching: true };
+    };
   }
 
   register() {
