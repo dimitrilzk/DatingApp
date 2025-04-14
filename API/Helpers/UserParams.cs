e53419776efb637e37fdd85847c3ef05094f46fe
dimitri_dev <dimitrilazuka95@gmail.com>
2025-04-14 21:30:33 +0200

    add min max age in query

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -15,5 +15,7 @@
 
         public string? Gender { get; set; }
         public string? CurrentUsername { get; set; }
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 100;
     }
 }
