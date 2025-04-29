6088ba9a2c7d399c49a9e082e937b57b61176537
dimitri_dev <dimitrilazuka95@gmail.com>
2025-04-29 22:46:36 +0200

    fix

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -16,4 +16,5 @@ export class MemberCardComponent {
   //in realtà l'input qui sotto è un extension method del signal
   //quindi nel template bisogna chimarlo es: member()
   member = input.required<Member>();
+  //todo
 }
