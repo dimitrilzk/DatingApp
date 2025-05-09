300da60914e798b35c1f1de370a7cb6eafc46d32
dimitri_dev <dimitrilazuka95@gmail.com>
2025-05-09 22:02:13 +0200

    add order by

--- client/src/app/_models/userParams.ts
+++ client/src/app/_models/userParams.ts
@@ -1,9 +1,12 @@
+import { User } from './user';
+
 export class UserParams {
   gender: string;
   minAge = 18;
   maxAge = 99;
   pageNumbr = 1;
   pageSize = 5;
+  orderBy = 'lastActive';
 
   constructor(user: User | null) {
     this.gender = user?.gender === 'female' ? 'male' : 'female';
